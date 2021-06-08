d83dc8206a90d22b16cdeffbba7425705259b6d8
Stanley Mok <stanleymwork@gmail.com>
2021-06-08 22:28:02 +0800

    feat: add zip of single file functionality

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
 import java.io.File;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.util.zip.ZipOutputStream;
+import java.util.zip.ZipEntry;
+
 
 public class Main {
 	public static void main(String[] args) {
@@ -22,18 +28,34 @@ public class Main {
     static void listFiles(final File folder, String inputDir, String outputDir, int maxSize) {
         for (final File fileEntry : folder.listFiles()) {
             if (fileEntry.isDirectory()) {
-                listFiles(fileEntry, inputDir, outputDir, maxSize);
+                listFiles(new File(fileEntry.getAbsolutePath()), 
+                                    fileEntry.getAbsolutePath(), outputDir, maxSize);
             } else {
                 System.out.println(fileEntry.getName());
-                compressFile(fileEntry, inputDir, outputDir, maxSize);
+                compressFile(fileEntry.getName(), inputDir, outputDir, maxSize);
             }
         }
     }
 
-    static void compressFile(final File file, String inputDir, String outputDir, int maxSize) {
-        System.out.println("compress code");
-        // compress from input dir/file to output dir/file
-
+    static void compressFile(String fileName, String inputDir, String outputDir, int maxSize) {
+        try {
+            String outFileName = outputDir + "/" + fileName + ".zippy1";
+            FileOutputStream fileOutStream = new FileOutputStream(outFileName);
+            ZipOutputStream zipOut = new ZipOutputStream(fileOutStream);
+            File fileToZip = new File(inputDir + "/" + fileName);
+            FileInputStream fileInStream = new FileInputStream(fileToZip);
+            ZipEntry zipEntry = new ZipEntry(outFileName);
+            zipOut.putNextEntry(zipEntry);
+            byte[] bytes = new byte[1024];
+            int length;
+            while((length = fileInStream.read(bytes)) >= 0)
+                zipOut.write(bytes, 0, length);
+            zipOut.close();
+            fileInStream.close();
+            fileOutStream.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 
     static void decompressDir(String inputDir, String outputDir) {
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 rm Main.class
-javac Main.java && java Main /mnt/c/Users/Stanley_Mok/OneDrive/Desktop/to_comp 2 3
+javac Main.java && java Main /mnt/c/Users/Stanley_Mok/OneDrive/Desktop/to_comp /mnt/c/Users/Stanley_Mok/OneDrive/Desktop/to_decomp 5
