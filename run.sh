8210ab24cb854e3831f8b7ec1e8d0668d7b5976c
Stanley Mok <stanleymwork@gmail.com>
2021-06-12 18:24:24 +0800

    test: reset folder struct

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+./test/run_tests.sh
 javac Main.java && \
 java Main /mnt/c/Users/Stanley_Mok/OneDrive/Desktop/to_comp /mnt/c/Users/Stanley_Mok/OneDrive/Desktop/to_decomp 0.5  && \
 java Main /mnt/c/Users/Stanley_Mok/OneDrive/Desktop/to_decomp /mnt/c/Users/Stanley_Mok/OneDrive/Desktop/to_comp3
+rm src/*.class *.class
