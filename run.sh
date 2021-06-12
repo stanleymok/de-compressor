b5e5480b286763f8a29a85ee7c2827f804776108
Stanley Mok <stanleymwork@gmail.com>
2021-06-12 18:41:26 +0800

    test: add scripts for testS

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
-./test/run_tests.sh
-javac Main.java && \
+# Compile and run unit tests
+echo "Compiling Source Files..." && javac Main.java && echo "Compilation Done." && \
+cd test && ./run_tests.sh && cd -
 java Main /mnt/c/Users/Stanley_Mok/OneDrive/Desktop/to_comp /mnt/c/Users/Stanley_Mok/OneDrive/Desktop/to_decomp 0.5  && \
 java Main /mnt/c/Users/Stanley_Mok/OneDrive/Desktop/to_decomp /mnt/c/Users/Stanley_Mok/OneDrive/Desktop/to_comp3
 rm src/*.class *.class
--- test/run_tests.sh
+++ test/run_tests.sh
@@ -2,5 +2,5 @@ export LIB_FOLDER=lib
 export JUNIT_HOME=$(pwd)/$LIB_FOLDER
 export CLASSPATH=$CLASSPATH:$JUNIT_HOME/junit-4.13.2.jar
 export CLASSPATH=$CLASSPATH:$JUNIT_HOME/hamcrest-2.2.jar
-echo "Compiling..." && javac *.java && echo "Compilation Successful." \
+echo "Compiling Test Files..." && javac *.java && echo "Compilation Successful." \
 	&& echo "Running JUnit Tests..." && java TestRunner && echo "Tests Successful."
